a2bb9096338b4bd9d0932c03a6794c63f3156374
viimunizz <vitoriamuniz0507@gmail.com>
2024-04-17 16:11:50 -0300

    commit

--- src/pages/Cadastro/Cadastro.jsx
+++ src/pages/Cadastro/Cadastro.jsx
@@ -3,35 +3,75 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import './Cadastro.css';
+import { Link } from 'react-router-dom';
 
 function Cadastro() {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="#home"><img src="https://sindusconsp.com.br/wp-content/uploads/2020/09/Logo_SENAI_PRINCIPAL_VERMELHO.png" width="100px" /></Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#Login">Login</Nav.Link>
-            <Nav.Link href="#Cadastro">Cadastro</Nav.Link>
-            <Nav.Link href="#PaginaInicial">Página Inicial</Nav.Link>
-            <Nav.Link href="#Turmas">Turmas</Nav.Link>
-            <Nav.Link href="#Aluno">Aluno</Nav.Link>
-            <NavDropdown title="Configurações" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Administrador</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Agenda
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Ocorrências</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Fale conosco☎️
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <div className="body">
+      <Navbar expand="lg" className="cadastro">
+        <Container>
+          <Navbar.Brand href="#home">
+            <img
+              src="https://sindusconsp.com.br/wp-content/uploads/2020/09/Logo_SENAI_PRINCIPAL_VERMELHO.png"
+              width="200px"
+            />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Link to="/">Login</Link>
+              <Nav.Link href="#Cadastro">Cadastro</Nav.Link>
+              <Nav.Link href="#PaginaInicial">Página Inicial</Nav.Link>
+              <Nav.Link href="#Turmas">Turmas</Nav.Link>
+              <Nav.Link href="#Aluno">Aluno</Nav.Link>
+              <NavDropdown title="Configurações" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">
+                  Administrador
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Agenda</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Ocorrências
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Fale conosco☎️
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+          <Navbar.Brand href="#home">
+            <img
+              className="Brasil"
+              src="https://static.mundoeducacao.uol.com.br/mundoeducacao/conteudo_legenda/5881291177d6af418ec43c3c95e05056.jpg"
+              width="50px"
+            />
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
+
+      <div className="caixa">
+        <form action="" className="formCadastro">
+          <div className="Login">
+            <h1>CADASTRO</h1>
+            <div className="formulario">
+              <label htmlFor="">Nome:</label>
+              <input type="text" placeholder="Nome:" />
+              <br />
+              <label htmlFor="">Email:</label>
+              <input type="text" placeholder="Email:" /><br />
+              <label htmlFor="">Senha:</label>
+              <input type="password" placeholder="Senha:" /><br />
+              <label htmlFor="">Confirmar senha:</label>
+              <input type="password" placeholder="Confirmar senha:" /><br /> 
+  
+              <div className="divButtonEntrar">
+                <button className="buttonEntrar">Entrar</button>
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
   );
 }
 
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -2,18 +2,21 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import './style.css';
+import './Login.css';
+import { Link } from 'react-router-dom';
 
 function Login()  {
   return (
+
+    <div className='body'>
     <Navbar expand="lg" id='login'>
       <Container>
-        <Navbar.Brand href="#home"><img src="https://sindusconsp.com.br/wp-content/uploads/2020/09/Logo_SENAI_PRINCIPAL_VERMELHO.png" width="100px" /></Navbar.Brand>
+        <Navbar.Brand href="#home"><img src="https://sindusconsp.com.br/wp-content/uploads/2020/09/Logo_SENAI_PRINCIPAL_VERMELHO.png" width="200px" /></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="#Login">Login</Nav.Link>
-            <Nav.Link href="#Cadastro">Cadastro</Nav.Link>
+            <Link className='cadastro-nav' to="/cadastro">Cadastro</Link>
             <Nav.Link href="#PaginaInicial">Página Inicial</Nav.Link>
             <Nav.Link href="#Turmas">Turmas</Nav.Link>
             <Nav.Link href="#Aluno">Aluno</Nav.Link>
@@ -30,10 +33,33 @@ function Login()  {
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
+        <Navbar.Brand href="#home"><img className='Brasil' src="https://static.mundoeducacao.uol.com.br/mundoeducacao/conteudo_legenda/5881291177d6af418ec43c3c95e05056.jpg" width="50px" /></Navbar.Brand>
+
       </Container>
     </Navbar>
+
+    <div className='caixa'>
+      <form action="" className='formLogin'>
+        <div className='Login'>
+          <h1>LOGIN</h1>
+          <div className='formulario'>
+            <label htmlFor="">Usuário:</label>
+            <input type="text" placeholder='Usuário:'/>
+            <br />
+            <label htmlFor="">Senha:</label>
+            <input type="password" placeholder='Senha:'/>
+
+            <div className='naoCadastro'><Link className="cadastro-style" to="/cadastro">Não tem um cadastro? Cadastra-se agora</Link></div><br />
+            <div className='divButtonEntrar'><button className='buttonEntrar'>Entrar</button></div>
+          </div>
+        </div>
+      </form>
+    </div>
+    </div>
+
+
   );
-  <div></div>
+  
 }
 
 export default Login;
